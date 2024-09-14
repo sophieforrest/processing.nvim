00b16738a90df42e04b8b1fb80edc61e3b7aff9b
piperinnshall <piper.innshall@gmail.com>
2024-09-14 20:47:30 +1200

    chore(config): set non-nil typing to config

--- lua/processing/config/init.lua
+++ lua/processing/config/init.lua
@@ -1,4 +1,3 @@
----@class processing.Config: processing.Opts
 ---@mod processing.config processing.nvim configuration.
 ---
 ---@brief [[
@@ -7,27 +6,29 @@
 ---
 --- You can set processing.nvim configuration options via `vim.g.processing_nvim`.
 ---
---->lua
---- ---@type processing.Config
---- vim.g.processing_nvim
----<
----
----@brief ]]
+---@endbrief
+
+---@class processing.Config: processing.Opts
+---@field highlight processing.Config.Highlight
+---@field lsp processing.Config.Lsp
+
+---@class processing.Config.Highlight
+---@field enable boolean
+
+---@class processing.Config.Lsp
+---@field cmd string[]|nil|fun(dispatchers: vim.lsp.rpc.Dispatchers): vim.lsp.rpc.PublicClient
 
 ---@tag vim.g.processing_nvim
 ---@tag g:processing_nvim
 ---@class processing.Opts
----@field highlight? processing.Opts.Highlight                                                  Syntax highlighting options.
----@field lsp? processing.Opts.Lsp                                                              Command to run to execute the processing language server.
+---@field highlight? processing.Opts.Highlight Syntax highlighting options.
+---@field lsp? processing.Opts.Lsp Command to run to execute the processing language server.
 
 ---@class processing.Opts.Highlight
----@field enable? boolean                                                                       Whether to enable treesitter highlighting with processing.
+---@field enable? boolean Whether to enable treesitter highlighting with processing.
 
 ---@class processing.Opts.Lsp
----@field cmd? string[]|fun(dispatchers: vim.lsp.rpc.Dispatchers): vim.lsp.rpc.PublicClient     Command to run for the Processing LSP. Will not run if option is not specified.
-
----@type processing.Config
-local config = nil
+---@field cmd? string[]|fun(dispatchers: vim.lsp.rpc.Dispatchers): vim.lsp.rpc.PublicClient Command to run for the Processing LSP. Will not run if option is not specified.
 
 ---@type processing.Opts
 vim.g.processing_nvim = vim.g.processing_nvim
@@ -36,15 +37,14 @@ local check = require("processing.config.check")
 local default_config = require("processing.config.default")
 local opts = vim.g.processing_nvim or {}
 
-config = vim.tbl_extend("force", default_config, opts)
+---@type processing.Config
+local config = vim.tbl_extend("force", default_config, opts)
 
----@cast config processing.Config
 local ok, err = check.validate(config)
 
 if not ok then
     ---@cast err string
     vim.notify(err, vim.log.levels.ERROR, { title = "processing.nvim" })
 end
-
 
 return config
