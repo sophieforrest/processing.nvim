355e0330b2ef3c827a8c5289e817784b1b86b774
Sophie Forrest <git@sophieforrest.com>
2024-09-14 18:14:47 +1200

    docs(check): fix incorrect name

--- lua/processing/config/check.lua
+++ lua/processing/config/check.lua
@@ -2,7 +2,7 @@
 ---
 ---@brief [[
 ---
---- rest.nvim config validation (internal)
+--- processing.nvim config validation (internal)
 ---
 ---@brief ]]
 
@@ -14,21 +14,21 @@ local Check = {}
 ---@return boolean is_valid
 ---@return string? error_message
 local function validate(tbl)
-    local prefix = "Invalid config: "
+    local prefix = 'Invalid config: '
     local ok, err = pcall(vim.validate, tbl)
     return ok or false, err and (prefix .. err) or nil
 end
----
+
 ---Validates the processing.nvim configuration.
 ---@param config processing.Config
 ---@return boolean is_valid
 ---@return string? error_message
 function Check.validate(config)
     local ok, err = validate({
-        highlight = { config.highlight, "table" },
-        ["highlight.enable"] = { config.highlight.enable, "boolean" },
-        lsp = { config.lsp, "table" },
-        ["lsp.cmd"] = { config.lsp.cmd, { "table", "function", "nil" } },
+        highlight = { config.highlight, 'table' },
+        ['highlight.enable'] = { config.highlight.enable, 'boolean' },
+        lsp = { config.lsp, 'table' },
+        ['lsp.cmd'] = { config.lsp.cmd, { 'table', 'function', 'nil' } },
     })
 
     if not ok then
